eaf2e9b9194d1c1fa293f4d725737cd663b6d9d3
VladShtefan1 <shtefanvlad@knu.ua>
2024-05-31 10:35:58 +0300

    added uris.py and corected views.py

--- recipe/views.py
+++ recipe/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Recipe, Category
 
-# Create your views here.
+def main(request):
+    latest_recipes = Recipe.objects.order_by('-created_at')[:5]
+    return render(request, 'main.html', {'latest_recipes': latest_recipes})
+
+def category_list(request):
+    categories = Category.objects.annotate(num_recipes=Count('recipe'))
+    return render(request, 'category_list.html', {'categories': categories})
